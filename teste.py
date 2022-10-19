9ff75173da8da2d8498031583d53e0281c44cde7
vanessa silva <vanessasilva@MacBook-Pro-de-vanessa.local>
2022-10-19 13:26:03 -0300

    primeiro commit.

---
+++ teste.py
@@ -0,0 +1,20 @@
+
+
+from re import X
+
+
+a = 0 
+for i in range(30):
+    if a % 2 == 0:
+        a += 1
+        continue
+    else:
+        if a % 5 ==0:
+            break
+        else:
+            a += 3
+        print(a)
+         
+        
+
+
